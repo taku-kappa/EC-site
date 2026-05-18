48096d8bf60f74ecf907c7e3eb16a73776bcb409
{taku-kappa} <{kamijimatakuya15@gmail.com}>
2026-05-18 22:42:18 +0900

    ポート番号を8080へ変更

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // ポート番号を8080へ変更
+  server: {
+      port: 8080,
+  },
 })
