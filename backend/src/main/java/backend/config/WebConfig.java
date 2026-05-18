1487ec18745d79b3ef829146ed9eee9ffd2de3c5
{taku-kappa} <{kamijimatakuya15@gmail.com}>
2026-05-18 22:41:51 +0900

    frontend側との通信内容記載

--- backend/src/main/java/backend/config/WebConfig.java
+++ backend/src/main/java/backend/config/WebConfig.java
@@ -10,11 +10,20 @@ public class WebConfig {
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
+
+            /**
+             * CORS（クロスオリジン通信）に関する具体的なルールを、registry に定義
+             *
+             * @param registry
+             */
             @Override
             public void addCorsMappings(CorsRegistry registry) {
+                // すべてのURL（API）に対してこのルールを適用する
                 registry.addMapping("/**")
+                        // 下記URL（オリジン）からのアクセスだけを信用して許可
                         .allowedOrigins("http://localhost:8080")
-                        .allowedMethods("*");
+                        // 下記の通信方法（HTTPメソッド）を許可
+                        .allowedMethods("GET", "POST", "PUT", "DELETE");
             }
         };
     }
