39a5ce2f344f267b5582e72a1df643980be9b965
{taku-kappa} <{kamijimatakuya15@gmail.com}>
2026-07-09 22:06:15 +0900

    ・RequestMappingアノテーション追加

--- backend/src/main/java/backend/controller/OrderController.java
+++ backend/src/main/java/backend/controller/OrderController.java
@@ -11,11 +11,13 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/api")
 @RequiredArgsConstructor
 public class OrderController {
 
